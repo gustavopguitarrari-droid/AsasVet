ae8a7e2a550b4d92975e48b9e736c1026a08e457
[dyad] <gustavopguitarrari@gmail.com>
2025-10-17 10:01:46 -0300

    [dyad] Atualizei o texto do rodapé para "Um desenvolvimento Agronegócios Guitarrari®". - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -2,7 +2,7 @@ export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center">
       <p className="text-sm text-gray-500 dark:text-gray-400">
-        Um desenvolvimento Agronegócios Guitarrari (Marca registrada)
+        Um desenvolvimento Agronegócios Guitarrari®
       </p>
     </div>
   );
