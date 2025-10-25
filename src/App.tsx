5ea0776c9b3bb0b93d1a0ab66e9699fc0af2d8e3
[dyad] <gustavopguitarrari@gmail.com>
2025-10-25 12:41:40 -0300

    [dyad] Habilitei as flags futuras do React Router v7 para resolver os avisos. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}> {/* Adicionado o prop 'future' */}
         <SessionContextProvider>
           <Routes>
             <Route path="/login" element={<Login />} />
