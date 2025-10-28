6d6419e6476bf406eed5fb01250af11a3e6ee0c0
[dyad] <gustavopguitarrari@gmail.com>
2025-10-28 19:05:07 -0300

    [dyad] Corrigindo erro SQL e de sintaxe JSX - wrote 1 file(s), executed 2 SQL queries

--- src/components/AddTransactionDialog.tsx
+++ src/components/AddTransactionDialog.tsx
@@ -98,7 +98,7 @@ const AddTransactionDialog: React.FC<AddTransactionDialogProps> = ({ onSubmit, o
           name="amount"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Valor (R$)</Label>
+              <FormLabel>Valor (R$)</FormLabel>
               <FormControl>
                 <div className="relative">
                   <DollarSign className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
