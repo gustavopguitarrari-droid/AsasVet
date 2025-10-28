c3d6ed2937b50d75cf7b98721ce94da906a1743c
[dyad] <gustavopguitarrari@gmail.com>
2025-10-28 20:09:25 -0300

    [dyad] Corrigi a ordem dos provedores em `src/main.tsx` para resolver o erro `useUser must be used within a UserProvider`. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import { ColorThemeProvider } from "./context/ColorThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="system" attribute="class" enableSystem>
-    <ColorThemeProvider>
-      <UserProvider> {/* UserProvider deve envolver o App */}
+    <UserProvider> {/* UserProvider agora envolve o ColorThemeProvider */}
+      <ColorThemeProvider>
         <App />
-      </UserProvider>
-    </ColorThemeProvider>
+      </ColorThemeProvider>
+    </UserProvider>
   </ThemeProvider>
 );
