60bad01a941723d810ead36996596f52384d279e
[dyad] <gustavopguitarrari@gmail.com>
2025-11-01 11:11:17 -0300

    [dyad] wrote 1 file(s)

--- src/components/AppointmentDetailsDialog.tsx
+++ src/components/AppointmentDetailsDialog.tsx
@@ -225,9 +225,11 @@ const AppointmentDetailsDialog: React.FC<AppointmentDetailsDialogProps> = ({
                 <Play className="mr-2 h-4 w-4" /> Iniciar Consulta
               </Button>
             )}
-            <Button variant="outline" onClick={() => setIsEditing(true)} className="w-full sm:w-auto mb-2 sm:mb-0" disabled={isFinalized}>
-              <Edit className="mr-2 h-4 w-4" /> Editar
-            </Button>
+            {!isFinalized && ( {/* Adicionado a condição aqui */}
+              <Button variant="outline" onClick={() => setIsEditing(true)} className="w-full sm:w-auto mb-2 sm:mb-0">
+                <Edit className="mr-2 h-4 w-4" /> Editar
+              </Button>
+            )}
             {!isFinalized && (
               <Button variant="destructive" onClick={handleCancelClick} className="w-full sm:w-auto">
                 <Trash2 className="mr-2 h-4 w-4" /> Cancelar Consulta
