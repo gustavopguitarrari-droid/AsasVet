ea671bab031c8a9aa210abb2305775ea93464ad5
[dyad] <gustavopguitarrari@gmail.com>
2025-11-06 23:20:12 -0300

    [dyad] Removendo espaçamento entre menu e páginas - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -73,7 +73,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <ResizablePanel>
         <div className="flex flex-1 flex-col h-full overflow-hidden"> {/* Adicionado h-full aqui */}
           <Header />
-          <div className="flex-1 overflow-y-auto p-6">
+          <div className="flex-1 overflow-y-auto"> {/* Removido p-6 daqui */}
             <main className="flex flex-col">{children}</main> {/* Removido min-h-full */}
           </div>
           <MadeWithDyad />
