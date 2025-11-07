784b2ad7895e8be112a1b815ed2f919f7faa9437
[dyad] <gustavopguitarrari@gmail.com>
2025-11-06 23:26:55 -0300

    Reverted all changes back to version c670dd3da5a1e36c3325ed7323ba8462c90a1ae4

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -73,7 +73,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <ResizablePanel>
         <div className="flex flex-1 flex-col h-full overflow-hidden"> {/* Adicionado h-full aqui */}
           <Header />
-          <div className="flex-1 overflow-y-auto py-4"> {/* Removido px-1, mantido py-4 */}
+          <div className="flex-1 overflow-y-auto px-1 py-4"> {/* Ajustado para px-1 py-4 */}
             <main className="flex flex-col">{children}</main> {/* Removido min-h-full */}
           </div>
           <MadeWithDyad />
