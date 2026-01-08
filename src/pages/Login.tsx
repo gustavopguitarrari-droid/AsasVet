dce05e2c6fad505f9e8c55e33a86150e327b01ef
[dyad] <gustavopguitarrari@gmail.com>
2026-01-08 18:24:44 -0300

    Reverted all changes back to version 7677779c90eff4477fd2857f76a4e4d97ea5871e

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -90,7 +90,7 @@ const Login = () => {
   return (
     <div className="min-h-screen w-full lg:grid lg:grid-cols-2 theme-nature-vet">
       <div className="hidden lg:flex relative flex-col items-center justify-end p-10 pb-20 text-white border-r-8 border-landingPage-lp-verde-folha-seca">
-        <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('/images/vet-clinic-background.png')" }} />
+        <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('/images/signup-background.png')" }} />
         <div className="absolute inset-0 bg-black/40" />
         
         <Link to="/" className="absolute top-8 left-8 flex items-center text-white z-10">
